53c2b7bcada7e30c591dffae1b7f070b8e7a57ba
maskimko <maskimko@ukr.net>
2015-02-16 01:17:55 +0200

    Shows table. Need jstl. Still need to format the output. And add a web service

--- src/main/java/ua/pp/msk/openvpnstatus/web/StatusBean.java
+++ src/main/java/ua/pp/msk/openvpnstatus/web/StatusBean.java
@@ -11,6 +11,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.Set;
 import javax.faces.bean.ManagedBean;
 //import javax.annotation.ManagedBean;
@@ -32,8 +33,9 @@ import ua.pp.msk.openvpnstatus.net.ManagementConnection;
 @ManagedBean()
 public class StatusBean {
 
-    private Status status;
-    private int port;
+    private static Status status;
+    private static Calendar actuality;
+    private int port = 0;
     private InetAddress host;
     private Connection mc;
 
@@ -44,59 +46,85 @@ public class StatusBean {
         this.port = port;
     }
 
-    public int getPort(){
+    public int getPort() {
         return port;
     }
-    
+
     public void setHost(String host) throws UnknownHostException {
         this.host = Inet4Address.getByName(host);
     }
-    public String getHost(){
-        
+
+    public String getHost() {
+
         return (host == null) ? "" : host.getHostName();
     }
 
-    
     public void setHost(InetAddress addr) {
         this.host = addr;
     }
 
-    private void checkStatus() throws OpenVpnParseException, OpenVpnIOException, IOException {
-        if (status == null) {
-            if (mc == null) {
-                mc = ManagementConnection.getConnection(host, port);
+    private synchronized void checkStatus() throws OpenVpnParseException, OpenVpnIOException, IOException {
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MINUTE, -1 * 1);
+        if (status == null || actuality == null || actuality.before(now)) {
+            try {
+                if (mc == null) {
+                    if (host == null) {
+                        ResourceBundle openVpnBundle = ResourceBundle.getBundle("server/OpenVpn");
+                        if (openVpnBundle.containsKey("host")) {
+                            host = InetAddress.getByName(openVpnBundle.getString("host"));
+                        }
+                    }
+                    if (port < 1) {
+                        ResourceBundle openVpnBundle = ResourceBundle.getBundle("server/OpenVpn");
+                        if (openVpnBundle.containsKey("port")) {
+                            port = Integer.parseInt(openVpnBundle.getString("port"));
+                        }
+                    }
+                    mc = ManagementConnection.getConnection(host, port);
+                }
+
+                if (!mc.isConnected()) {
+                    mc.connect();
+                }
+                status = mc.getStatus();
+                actuality = Calendar.getInstance();
+            } catch (IOException ex) {
+                LoggerFactory.getLogger(StatusBean.class.getName()).error("Connection IO error", ex);
+            } finally {
+                try {
+                    mc.close();
+                } catch (Exception ex) {
+                    LoggerFactory.getLogger(StatusBean.class.getName()).error("Cannot close connection", ex);
+                }
             }
-            if (!mc.isConnected()){
-                mc.connect();
-            }
-            status = mc.getStatus();
         }
     }
-    
-    public Status getStatus(){
+
+    public Status getStatus() {
         try {
-        checkStatus();
+            checkStatus();
         } catch (IOException ex) {
-             LoggerFactory.getLogger(StatusBean.class.getName()).warn("IO error", ex);
-        } catch (OpenVpnIOException ex ) {
-              LoggerFactory.getLogger(StatusBean.class.getName()).warn("VPN IO error", ex);
+            LoggerFactory.getLogger(StatusBean.class.getName()).warn("IO error", ex);
+        } catch (OpenVpnIOException ex) {
+            LoggerFactory.getLogger(StatusBean.class.getName()).warn("VPN IO error", ex);
         } catch (OpenVpnParseException ex) {
-             LoggerFactory.getLogger(StatusBean.class.getName()).warn("Parsing error", ex);
+            LoggerFactory.getLogger(StatusBean.class.getName()).warn("Parsing error", ex);
         }
         return status;
     }
 
     public Calendar getUpdateTime() {
-        Calendar ut = null; 
+        Calendar ut = null;
         try {
             checkStatus();
             ut = status.getUpdateTime();
         } catch (IOException ex) {
-             LoggerFactory.getLogger(StatusBean.class.getName()).warn("IO error", ex);
-        } catch (OpenVpnIOException ex ) {
-              LoggerFactory.getLogger(StatusBean.class.getName()).warn("VPN IO error", ex);
+            LoggerFactory.getLogger(StatusBean.class.getName()).warn("IO error", ex);
+        } catch (OpenVpnIOException ex) {
+            LoggerFactory.getLogger(StatusBean.class.getName()).warn("VPN IO error", ex);
         } catch (OpenVpnParseException ex) {
-             LoggerFactory.getLogger(StatusBean.class.getName()).warn("Parsing error", ex);
+            LoggerFactory.getLogger(StatusBean.class.getName()).warn("Parsing error", ex);
         }
         return ut;
     }
@@ -115,9 +143,9 @@ public class StatusBean {
         }
         return clientList;
     }
-    
-    public Set<Route> getRoutes(){
-         Set<Route> routes = null;
+
+    public Set<Route> getRoutes() {
+        Set<Route> routes = null;
         try {
             checkStatus();
             routes = status.getRoutes();
@@ -131,10 +159,10 @@ public class StatusBean {
         return routes;
     }
 
-    public String process(){
+    public String process() {
         return "status.xhtml";
     }
-    
+
 //    @PreDestroy
 //    public void tearDown() {
 //        if (mc != null) {
